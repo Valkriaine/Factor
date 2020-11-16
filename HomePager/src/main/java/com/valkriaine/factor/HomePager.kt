889500af12e48192c1a71a2166b95fc1c4cfe514
Yifei <valkriaine@outlook.com>
2020-11-15 17:28:06 -0800

    added setSwipeAllowed()

--- HomePager/src/main/java/com/valkriaine/factor/HomePager.kt
+++ HomePager/src/main/java/com/valkriaine/factor/HomePager.kt
@@ -10,39 +10,54 @@ import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 
 
-class HomePager(context: Context?, attrs: AttributeSet?) : ViewPager(context!!, attrs) {
+class HomePager(context: Context?, attrs: AttributeSet?) : ViewPager(context!!, attrs)
+{
+    private var isSwipeAllowed = true
     private var initialXValue = 0f
     private var diff = 0f
 
     private val views: ArrayList<View> = ArrayList()
     private val adapter: HomePagerAdapter = HomePagerAdapter(views)
 
-    override fun onAttachedToWindow() {
+    override fun onAttachedToWindow()
+    {
         super.onAttachedToWindow()
         this.offscreenPageLimit = views.count() - 1
         this.setAdapter(adapter)
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        return if (isSwipeAllowed(event)) {
+    override fun onTouchEvent(event: MotionEvent): Boolean
+    {
+        return if (isSwipeAllowed(event))
+        {
             super.onTouchEvent(event)
-        } else false
+        }
+        else false
     }
 
-    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
-        return if (isSwipeAllowed(event)) {
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean
+    {
+        return if (isSwipeAllowed(event))
+        {
             super.onInterceptTouchEvent(event)
         } else false
     }
 
-    private fun isSwipeAllowed(event: MotionEvent): Boolean {
+    private fun isSwipeAllowed(event: MotionEvent): Boolean
+    {
 
-        if (event.action == MotionEvent.ACTION_DOWN) {
+        if (!isSwipeAllowed)
+            return false
+
+        if (event.action == MotionEvent.ACTION_DOWN)
+        {
             initialXValue = event.x
             return true
         }
-        if (event.action == MotionEvent.ACTION_MOVE) {
+
+        if (event.action == MotionEvent.ACTION_MOVE)
+        {
             diff = event.x - initialXValue
             if (diff > 0 && this.currentItem == 0)
                 return false
@@ -50,17 +65,28 @@ class HomePager(context: Context?, attrs: AttributeSet?) : ViewPager(context!!, 
                 return false
         }
         return true
+
     }
 
-    override fun addView(view: View, position: Int) {
-        if (!views.contains(view)) {
+    fun setSwipeAllowed(allowed: Boolean)
+    {
+        this.isSwipeAllowed = allowed
+    }
+
+
+    override fun addView(view: View, position: Int)
+    {
+        if (!views.contains(view))
+        {
             views.add(position, view)
             adapter.notifyDataSetChanged()
         }
     }
 
-    override fun addView(view: View) {
-        if (!views.contains(view)) {
+    override fun addView(view: View)
+    {
+        if (!views.contains(view))
+        {
             views.add(view)
             adapter.notifyDataSetChanged()
         }
